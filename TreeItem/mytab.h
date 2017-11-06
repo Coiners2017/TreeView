e73dd58bac59e017d63f18cf8f53b47200785409
HSW <Coiners2017@outlook.com>
2017-11-06 21:59:54 +0800

    add mytable

--- TreeItem/mytab.h
+++ TreeItem/mytab.h
@@ -10,6 +10,15 @@
 #include <QWidget>
 #include <QIcon>
 #include <QComboBox>
+#include <QCheckBox>
+#include <QDir>
+#include <QFileInfo>
+#include <QDebug>
+#include <QListView>
+#include <QStandardItemModel>
+#include <QStandardItem>
+#include <QStringListModel>
+#include <QStringList>
 #define IDS_STRMACHFTPTYPE0 "标准"
 #define IDS_STRMACHFTPTYPE1 "三菱"
 #define IDS_STRMACHFTPTYPE2 "西门子"
@@ -18,10 +27,13 @@ class MyTab : public QDialog
     Q_OBJECT
 public:
     MyTab(QWidget *parent = 0);
+    void addFolder(QString path);
 signals:
 public slots:
 private:
     QTabWidget *tabWidget;
+    QListView *listView;
+    QStandardItemModel *standardItemModel;
 };
 
 #endif // MYTAB_H
