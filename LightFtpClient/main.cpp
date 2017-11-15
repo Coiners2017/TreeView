b9fc088df037a7ef00a3cae6fa534d2763101c7e
HSW <Coiners2017@outlook.com>
2017-11-15 17:26:21 +0800

    Modify LightFtpClient/main.cpp

--- LightFtpClient/main.cpp
+++ LightFtpClient/main.cpp
@@ -2,11 +2,12 @@
 #include <QApplication>
 #include "iptype.h"
 #include "myipwidget.h"
-
+#include "clientconn.h"
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    LightFtpClientDlg w;
-    w.show();
+    //LightFtpClientDlg w;
+    ClientConn conn;
+    //w.show();
     return a.exec();
 }
